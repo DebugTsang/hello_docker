4ba03f55e4d4136e48fa6805cc253800c0f2bc4d
Calvin Tsang <debug.tsang@gmail.com>
2020-01-10 14:38:53 +0800

    Init basic docker image

---
+++ READNE.md
@@ -0,0 +1,5 @@
+# Docker Build
+vi Dockerfile
+
+docker build -t hello_docker .
+docker run -p 4000:80 hello_docker
---
+++ app/hello_world.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import json
+
+data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
+
+json3 = json.dumps(data)
+print (json3)
+
